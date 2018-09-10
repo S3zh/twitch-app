0545aceca21f9145cf1d2dfe728e2a6d549ddb18
S3zh <32306528+S3zh@users.noreply.github.com>
2018-09-10 18:14:36 +0300

    Обработка ошибок в запросах, добавил отписку, правильно описал интерфейс, исправил баги.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import {StreamService} from './stream.service';
 import {User} from './user';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
 
   user: User;
+  private subject = new Subject();
+
 
   constructor(private streamService: StreamService) {}
 
@@ -18,10 +22,15 @@ export class AppComponent implements OnInit {
     this.checkUser();
   }
 
+  ngOnDestroy(){
+    this.subject.next(true);
+    this.subject.complete();
+  }
+
   checkUser() {
-    this.streamService.checkOaut().subscribe(
-      (answer: object) => {
-        this.user = answer['token'];
+    this.streamService.checkOaut().pipe(takeUntil(this.subject)).subscribe(
+      (answer: any) => {
+        this.user = answer.token;
       });
   }
 
